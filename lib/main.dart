7439f81c0652258af9e8a6899b82dbfe5374ffaa
IvanKhartanovich <ivan_khart@mail.ru>
2024-06-17 10:44:27 +0300

    some refactoring and logic for buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'index.dart';
 
 void main() {
@@ -11,9 +10,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainPage(),
+      home: Theme(
+        data: ThemeData(fontFamily: 'Inter'),
+        child: const MainPage(),
+      ),
     );
   }
 }
---
+++ lib/presentation/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  static const defaultTextColor = Color(0xFFFFFFFF);
+  static const backgroundColor = Color(0xFF151515);
+  static const dividerColor = Color(0xFF4E4D4D);
+  static const redColor = Color(0xFFFF5959);
+  static const greenColor = Color(0xFF66FF7F);
+  static const greyColor = Color(0xFF343434);
+  static const lightGreyColor = Color(0xFF969696);
+}
